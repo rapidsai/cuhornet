5091e474c8636aa6f576f90e97271f332fd7f68f
Oded Green <ogreen@nvidia.com>
2019-07-24 10:59:12 -0400

    Removed floating point test

--- hornetsnest/test/BCMultiTest.cu
+++ hornetsnest/test/BCMultiTest.cu
@@ -25,85 +25,16 @@ using namespace graph::parsing_prop;
 
 #include <cub/cub.cuh> 
 
-
-#include <thrust/random.h>
-#include <thrust/iterator/counting_iterator.h>
-#include <thrust/functional.h>
-#include <thrust/transform_reduce.h>
-
-
 #include <thrust/device_vector.h>
 #include <thrust/transform.h>
 #include <thrust/execution_policy.h>
 #include <thrust/system/cuda/execution_policy.h>
 #include <thrust/iterator/counting_iterator.h>
-#include <thrust/copy.h>
-#include <thrust/sort.h>
-#include <thrust/random/linear_congruential_engine.h>
-#include <thrust/random/uniform_real_distribution.h>
-
-//fill vector w/ random values;
-//(specialization for floating point types)
-//
-template<typename T, typename IndexT = int64_t>
-// std::enable_if<std::is_floating_point<T>::value>
-void
-random_fill(thrust::device_vector<T>& d_v, IndexT seed, T lb, T ub)
-{
-  thrust::counting_iterator<T> index_sequence_begin(seed);
-
-  thrust::transform(thrust::device, index_sequence_begin, index_sequence_begin + d_v.size(),
-                    d_v.begin(),
-                    [lb, ub] __host__ __device__(T index) { //okay
-                      // thrust::minstd_rand rng(index);
-                       thrust::default_random_engine rng;
-                      thrust::uniform_real_distribution<T> dist(lb, ub);
-                      // thrust::uniform_real_distribution<T> dist(lb, ub);
-                      rng.discard(index);
-                      // return double(index+1) * dist(rng);
-                      return 10000000.0/(double(index+1));
-                    });
-
-  //debug:
-  //std::cout<<"double specialization...\n";
-}
-
-int floatTest() {
-
-    const int N = 1<<25;
-    int64_t seed = 0;
-    thrust::device_vector<double> V1(N);
-    thrust::device_vector<double> V2(N);
-    thrust::device_vector<double> V3(N);
-
-    random_fill(V1,seed,0.0,double(1));
-    random_fill(V2,seed,0.0,double(1));
-
-    // thrust::transform(thrust::device,V1.begin(), V1.end(), V2.begin(), V3.begin(), thrust::multiplies<double>());                
-    thrust::transform(thrust::device,V1.begin(), V1.end(), V2.begin(), V3.begin(), thrust::plus<double>());                
-
-    double sumS = thrust::reduce(thrust::device,  V3.begin(), V3.end(),0.0);
-
-    cout << setprecision (25) << "double value - unsorted : " << sumS << endl;
-
-    thrust::sort(thrust::device,V3.begin(), V3.end());
-
-    sumS = thrust::reduce(thrust::device,  V3.begin(), V3.end(),0.0);
-
-    cout << setprecision (25) << "double value -   sorted : " << sumS << endl;
-
-
-    return 0;
-}
 
 int main(int argc, char* argv[]) {
     using namespace timer;
     using namespace hornets_nest;
 
-    // for(int k=0; k<5;k++){
-    //     floatTest();
-    // }
-    // return 1;
 
     // GraphStd<vid_t, eoff_t> graph(UNDIRECTED);
     graph::GraphStd<vid_t, eoff_t> graph;
