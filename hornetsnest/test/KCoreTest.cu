3d7c81220535e4792e034e1216689144590756c5
Kumar Aatish <kaatish@nvidia.com>
2019-09-24 00:14:12 -0400

    WIP KCore integration

--- hornetsnest/test/KCoreTest.cu
+++ hornetsnest/test/KCoreTest.cu
@@ -16,14 +16,14 @@ int main(int argc, char **argv) {
     using namespace graph::structure_prop;
     using namespace graph::parsing_prop;
 
-    graph::GraphStd<vid_t, eoff_t> graph(UNDIRECTED);
+    graph::GraphStd<vert_t, eoff_t> graph(UNDIRECTED);
     graph.read(argv[1], SORT | PRINT_INFO);
 
     HornetInit hornet_init(graph.nV(), graph.nE(), graph.csr_out_offsets(),
                            graph.csr_out_edges());
 
     HornetGraph hornet_graph(hornet_init);
-    KCore kcore(hornet_graph);
+    KCoreDynamic kcore(hornet_graph);
     kcore.run();
 
 }
