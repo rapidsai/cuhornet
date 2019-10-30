3961781c6c2f712769b5b11494783862c83c8e77
Oded Green <ogreen@nvidia.com>
2019-10-30 13:13:12 -0400

    cleaning up dead code

--- hornetsnest/src/Static/KTruss/KTrussOperators.cuh
+++ hornetsnest/src/Static/KTruss/KTrussOperators.cuh
@@ -11,7 +11,6 @@ struct Init {
     }
 };
 
-//------------------------------------------------------------------------------
 
 struct FindUnderK {
     HostDeviceVar<KTrussData> kt;
@@ -30,11 +29,8 @@ struct FindUnderK {
             if (kt().triangles_per_edge[pos] < (kt().max_K - 2)) {
                 int       spot = atomicAdd((kt().counter), 1);
                 kt().src[spot] = src;
-                // auto edge = vertex.edge(adj);
-                // kt().dst[spot] = edge.dst_id();
                 vert_t dest = vertex.neighbor_ptr()[adj];
                 kt().dst[spot] = dest;
-
             }
         }
     }
@@ -57,14 +53,11 @@ struct SimpleBubbleSort {
             vert_t temp = vertex.neighbor_ptr()[i];
             vertex.neighbor_ptr()[i] = vertex.neighbor_ptr()[min_idx];
             vertex.neighbor_ptr()[min_idx] = temp;
-
-
         }
  
     }
 };
 
-//------------------------------------------------------------------------------
 
 struct CountActive {
     HostDeviceVar<KTrussData> kt;
@@ -78,60 +71,5 @@ struct CountActive {
             atomicAdd((kt().active_vertices), 1);
     }
 };
-
-//------------------------------------------------------------------------------
-
-
-// struct FindUnderKDynamic {
-//     HostDeviceVar<KTrussData> kt;
-
-//     OPERATOR(Vertex& vertex) {
-//         vert_t src = vertex.id();
-
-//         if(kt().is_active[src] == 0)
-//             return;
-//         if(vertex.degree() == 0) {
-//             kt().is_active[src] = 0;
-//             return;
-//         }
-//         for (vert_t adj = 0; adj < vertex.degree(); adj++) {
-//             auto edge = vertex.edge(adj);
-//             if (edge.weight() < kt().max_K - 2) {
-//                 int       spot = atomicAdd(&(kt().counter), 1);
-//                 kt().src[spot] = src;
-//                 kt().dst[spot] = edge.dst_id();
-//             }
-//         }
-//     }
-// };
-
-//------------------------------------------------------------------------------
-
-// struct QueueActive {
-//     HostDeviceVar<KTrussData> kt;
-
-//     OPERATOR(Vertex& vertex) {
-//         vert_t src = vertex.id();
-
-//         if (vertex.degree() == 0 && !kt().is_active[src])
-//             kt().is_active[src] = 0;
-//         else
-//             kt().active_queue.insert(src);
-//     }
-// };
-
-//------------------------------------------------------------------------------
-
-
-// struct ResetWeights {
-//     HostDeviceVar<KTrussData> kt;
-
-//     OPERATOR(Vertex& vertex) {
-//         int pos = kt().offset_array[vertex.id()];
-
-//         for (vert_t adj = 0; adj < vertex.degree(); adj++)
-//             vertex.edge(adj).set_weight(kt().triangles_per_edge[pos + adj]); //!!!
-//     }
-// };
 
 } // namespace hornets_nest
