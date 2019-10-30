3d4b336bfc8f3957955fa0e1fb9c12878f78d72b
Oded Green <ogreen@nvidia.com>
2019-10-30 13:13:12 -0400

    cleaning up dead code

--- hornetsnest/src/Static/KTruss/KTruss.cu
+++ hornetsnest/src/Static/KTruss/KTruss.cu
@@ -84,8 +84,6 @@ void KTruss::resetVertexArray() {
 }
 
 void KTruss::resetEdgeArray() {
-
-    // gpu::memsetZero(hd_data().triangles_per_edge, hornet.nE());
     gpu::memsetZero(hd_data().triangles_per_edge, originalNE);
 }
 
@@ -113,20 +111,9 @@ void KTruss::run() {
     int  iterations = 0;
 
     while (true) {
-        // if(hd_data().max_K >= 1000)
-        //     break;
-        //std::cout << hd_data().num_edges_remaining << std::endl;
         bool need_stop = false;
 
-        // printf("Number of remaining edges %ld\n",hd_data().num_edges_remaining);
-
         bool      more = findTrussOfK(need_stop);
-
-        //if (more == false && need_stop) {
-        // if(hornet.nE()==0){
-        //     hd_data().max_K--;
-        //     break;
-        // }
 
         if (hd_data().num_edges_remaining <= 0) {
             hd_data().max_K--;
@@ -136,10 +123,10 @@ void KTruss::run() {
 
         iterations++;
     }
-    std::cout << "iterations " << iterations << std::endl;
-    cout << "Found the maximal KTruss at : " << hd_data().max_K << endl;
-    std::cout << "The number of full triangle counting iterations is  : "
-             << hd_data().full_triangle_iterations << std::endl;
+    // std::cout << "iterations " << iterations << std::endl;
+    // cout << "Found the maximal KTruss at : " << hd_data().max_K << endl;
+    // std::cout << "The number of full triangle counting iterations is  : "
+    //          << hd_data().full_triangle_iterations << std::endl;
 }
 
 void KTruss::runForK(int max_K) {
@@ -157,15 +144,10 @@ bool KTruss::findTrussOfK(bool& stop) {
     cudaMemset(hd_data().counter,0, sizeof(int));//hd_data().counter = 0
 
     int h_active_vertices = originalNV;
-    // cudaMemcpy(hd_data().active_vertices,&h_active_vertices, sizeof(int),cudaMemcpyHostToDevice);
 
-    // int sum_deleted_edges = 0;
     stop = true;
 
     while (h_active_vertices > 0) {
-
-
-        // std::cout << "MaxK =  " << hd_data().max_K << std::endl;
 
         hd_data().full_triangle_iterations++;
 
@@ -182,17 +164,10 @@ bool KTruss::findTrussOfK(bool& stop) {
         int h_counter;
         cudaMemcpy(&h_counter,hd_data().counter, sizeof(int),cudaMemcpyDeviceToHost);
 
-        // std::cout << "Current number of deleted edges is " << h_counter << std::endl;
-
         if (h_counter != 0) {
               UpdatePtr ptr(h_counter, hd_data().src, hd_data().dst);
               Update batch_update(ptr);
               hornet.erase(batch_update);
-
-            // BatchUpdate batch_update(hd_data().src, hd_data().dst,
-            //                          hd_data().counter, gpu::BatchType::DEVICE);
-
-            // hornet.deleteEdgeBatch(batch_update);
         }
         else{
             return false;
@@ -209,16 +184,7 @@ bool KTruss::findTrussOfK(bool& stop) {
         // Getting the number of active vertices
 
         cudaMemcpy(&h_active_vertices, hd_data().active_vertices,sizeof(int),cudaMemcpyDeviceToHost);
-        // printf("Number of active vertices %d\n",h_active_vertices);
 
-        // CHECK_ERROR("Crashing after counting")
-
-        // if (h_counter == 0) 
-        //     return false;
-
-
-
-        // hd_data.sync();
         resetEdgeArray();
         resetVertexArray();
 
@@ -232,125 +198,5 @@ bool KTruss::findTrussOfK(bool& stop) {
     return true;
 }
 
-//==============================================================================
-//==============================================================================
-//==============================================================================
-
-// void KTruss::runDynamic(){
-//     hd_data().max_K = 3;
-//     forAllVertices(hornet, Init { hd_data });
-
-//     resetEdgeArray();
-//     resetVertexArray();
-
-//     kTrussOneIteration(hornet, hd_data().triangles_per_vertex,
-//                            hd_data().tsp, hd_data().nbl,
-//                            hd_data().shifter,
-//                            hd_data().blocks, hd_data().sps,
-//                            hd_data);   //sub
-//     hd_data.sync();
-//     forAllVertices(hornet, ResetWeights { hd_data });
-
-//     int iterations = 0;
-//     while (true) {
-//         //if(hd_data().max_K >= 5)
-//         //    break;
-//         //std::cout << "New iteration" << std::endl;
-//         bool need_stop = false;
-//         bool     more = findTrussOfKDynamic(need_stop);
-//         CHECK_CUDA_ERROR
-//         //std::cout << hd_data().num_edges_remaining << std::endl;
-//         //if (more == false && need_stop) {
-//         if (hd_data().num_edges_remaining <= 0) {
-//             hd_data().max_K--;
-//             break;
-//         }
-//         hd_data().max_K++;
-//         iterations++;
-//     }
-//     //std::cout << "iterations " << iterations << std::endl;
-// }
-
-// bool KTruss::findTrussOfKDynamic(bool& stop) {
-//     hd_data().counter = 0;
-//     hd_data().active_queue.clear();  //queue
-
-//     forAllVertices(hornet, QueueActive { hd_data }); //queue
-//     forAllVertices(hornet, CountActive { hd_data });
-//     hd_data.sync();
-//     hd_data().active_queue.swap();//queue
-
-//     stop = true;
-//     while (hd_data().active_vertices > 0) {
-//         forAllVertices(hornet, hd_data().active_queue,
-//                        FindUnderKDynamic { hd_data });    //queue
-//         hd_data.sync();
-//         //std::cout << "Current number of deleted edges is "
-//         //<< hd_data().counter << std::endl;
-
-//         /*if (hd_data().counter == hd_data().num_edges_remaining) {
-//             stop = true;
-//             return false;
-//         }*/
-//         if (hd_data().counter != 0) {
-//             //directly on the device
-//             //auto src_array = new vert_t[hd_data().counter];
-//             //auto dst_array = new vert_t[hd_data().counter];
-//             //cuMemcpyToHost(hd_data().src, hd_data().counter,
-//             //               src_array);
-//             //cuMemcpyToHost(hd_data().dst, hd_data().counter,
-//             //               dst_array);
-//             //hornet::BatchInit batch_init(src_array, dst_array,
-//             //                                hd_data().counter);
-//             BatchUpdate batch_update(hd_data().src, hd_data().dst,
-//                                      hd_data().counter, gpu::BatchType::DEVICE);
-//             //batch_update.sendToDevice(batch_init);//directly on the device
-//             hornet.deleteEdgeBatch(batch_update);
-
-//             callDeviceDifferenceTriangles(hornet, batch_update,
-//                                           hd_data().triangles_per_vertex,
-//                                           hd_data().tsp,
-//                                           hd_data().nbl,
-//                                           hd_data().shifter,
-//                                           hd_data().blocks,
-//                                           hd_data().sps, true);
-//         }
-//         else
-//             return false;
-
-//         hd_data().num_edges_remaining -= hd_data().counter;
-//         hd_data().active_vertices = 0;
-//         hd_data().counter         = 0;
-
-//         //allVinA_TraverseVertices<ktruss_operators::countActive>
-//         //    (hornet, deviceKTrussData, hd_data().active_queue.getQueue(),
-//         //     activeThisIteration);
-
-//         forAllVertices(hornet, hd_data().active_queue, CountActive { hd_data });
-
-//         hd_data.sync();
-//         stop = false;
-//     }
-//     return true;
-// }
-
-// void KTruss::runForKDynamic(int max_K) {
-//     hd_data().max_K = max_K;
-
-//     forAllVertices(hornet, Init { hd_data });
-
-//     resetEdgeArray();
-//     resetVertexArray();
-
-//     kTrussOneIteration(hornet, hd_data().triangles_per_vertex, 4,
-//                        hd_data().sps / 4, 2, hd_data().blocks,
-//                        hd_data().sps, hd_data);
-//     hd_data.sync();
-
-//     forAllVertices(hornet, ResetWeights { hd_data });
-
-//     bool need_stop = false;
-//     bool      more = findTrussOfKDynamic(need_stop);
-// }
 
 } // hornet_alg namespace
