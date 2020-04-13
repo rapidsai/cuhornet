e2c0d505f51a56ff0d934704e296427112f66ce9
Oded Green <ogreen@nvidia.com>
2020-04-13 13:10:00 -0400

    1) corrected deallocation, 2) replaced calls to skinny worker, 3) cleaned up code base

--- primitives/LoadBalancing/lrb_lb.i.cuh
+++ primitives/LoadBalancing/lrb_lb.i.cuh
@@ -54,38 +54,34 @@ namespace hornets_nest {
 namespace load_balancing {
 
 template<typename HornetClass>
-LogarthimRadixBinning32::LogarthimRadixBinning32(HornetClass& hornet,
-                           const float work_factor) noexcept {
-    //static_assert(IsHornet<HornetClass>::value,
-    //             "LogarthimRadixBinning32: parameter is not an instance of Hornet Class");
-    // d_work.resize(work_factor * hornet.nV());
-    // prefixsum.resize(work_factor * hornet.nV());
+LogarthimRadixBinning32::LogarthimRadixBinning32(HornetClass& hornet) noexcept {
 
-    gpu::allocate(d_lrbRelabled, hornet.nV());
+    d_lrbRelabled = nullptr;
+    if(hornet.nV()>0)
+        gpu::allocate(d_lrbRelabled, hornet.nV());
     gpu::allocate(d_bins, 33);
     gpu::allocate(d_binsPrefix, 33);
 
     cudaEventCreate(&syncher);
 
-    streams = new cudaStream_t[STREAMS];
-    for(int i=0;i<STREAMS; i++)
+    streams = new cudaStream_t[LRB_STREAMS];
+    for(int i=0;i<LRB_STREAMS; i++)
       cudaStreamCreate ( &(streams[i]));    
-
 }
 
 inline LogarthimRadixBinning32::~LogarthimRadixBinning32() noexcept {
     //hornets_nest::gpu::free(_d_work);
-    if(d_lrbRelabled == nullptr){
+    if(d_lrbRelabled != nullptr){
         gpu::free(d_lrbRelabled);d_lrbRelabled = nullptr;
     }
-    if(d_bins == nullptr){
+    if(d_bins != nullptr){
         gpu::free(d_bins);d_bins = nullptr;
     }
-    if(d_binsPrefix == nullptr){
+    if(d_binsPrefix != nullptr){
         gpu::free(d_binsPrefix);d_binsPrefix = nullptr;
     }
 
-    for(int i=0;i<STREAMS; i++)
+    for(int i=0;i<LRB_STREAMS; i++)
       cudaStreamDestroy ( streams[i]);
 
     cudaEventDestroy(syncher);
@@ -98,12 +94,6 @@ void LogarthimRadixBinning32::apply(HornetClass& hornet,
                          const vid_t *      d_input,
                          int                num_vertices,
                          const Operator&    op) const noexcept {
-    //static_assert(IsHornet<HornetClass>::value,
-    //             "LogarthimRadixBinning32: paramenter is not an instance of Hornet Class");
-    // prefixsum.resize(num_vertices + 1);
-    // int ITEMS_PER_BLOCK = xlib::DeviceProperty
-    //                       ::smem_per_block<vid_t>(BLOCK_SIZE);
-    //assert(num_vertices < _work_size && "LogarthimRadixBinning32 (work queue) too small");
 
     cudaMemset(d_bins,0,33*sizeof(vid_t));
 
@@ -133,13 +123,6 @@ void LogarthimRadixBinning32::apply(HornetClass& hornet,
     }
 
 
-
-    // printf("The number of really fat vertices is :");
-    // for(int i=0; i<20; i++){
-    //     printf("%d, ",h_binsPrefix[i+1]-h_binsPrefix[i]);
-    // }
-    // printf("\n");
-
     int activeVertices = h_binsPrefix[16];
     int blockSize = 32;
     if(activeVertices>0){
@@ -148,15 +131,13 @@ void LogarthimRadixBinning32::apply(HornetClass& hornet,
 
 
     activeVertices = h_binsPrefix[20] - h_binsPrefix[16];
-    // activeVertices = h_binsPrefix[20];
 
     blockSize = 1024;
     if(activeVertices>0){
         kernel::fatWorkerLRB<<<activeVertices,blockSize,0,streams[1]>>>(hornet.device(), d_lrbRelabled, op, activeVertices,h_binsPrefix[16]);
-        // kernel::fatWorkerLRB<<<activeVertices,blockSize,0,streams[1]>>>(hornet.device(), d_lrbRelabled, op, activeVertices,0);
     }
 
-    const int remainingFat=9;
+    const int remainingFat=12;
     const int bi = 20+remainingFat-1;
 
     blockSize = 512;
@@ -174,13 +155,16 @@ void LogarthimRadixBinning32::apply(HornetClass& hornet,
             blockSize=32;
     }
 
-    const int smallBlockSize = 32;
-    int smallVertices = num_vertices-h_binsPrefix[bi];
-    int smallVerticesBlocks = (smallVertices)/smallBlockSize + ((smallVertices%smallBlockSize)?1:0);
-    if(smallVerticesBlocks>0){                   
-        kernel::skinnyWorkerLRB<<<smallVerticesBlocks,smallBlockSize,0,streams[STREAMS-1]>>>( hornet.device(), d_lrbRelabled, op, smallVertices,h_binsPrefix[bi]); 
-    }
+    // const int smallBlockSize = 32;
+    // int smallVertices = num_vertices-h_binsPrefix[bi];
+    // int smallVerticesBlocks = (smallVertices)/smallBlockSize + ((smallVertices%smallBlockSize)?1:0);
+    // if(smallVerticesBlocks>0){                   
+    //     kernel::skinnyWorkerLRB<<<smallVerticesBlocks,smallBlockSize,0,streams[LRB_STREAMS-1]>>>( hornet.device(), d_lrbRelabled, op, smallVertices,h_binsPrefix[bi]); 
+    // }
     cudaEventSynchronize(syncher);
+
+    // cudaEventDestroy(syncher);
+
 }
 
 template<typename HornetClass, typename Operator>
@@ -193,71 +177,3 @@ void LogarthimRadixBinning32::apply(HornetClass& hornet, const Operator& op)
 } // namespace hornets_nest
 
 
-
-
-// template<typename HornetClass, typename Operator, typename vid_t>
-// void LogarthimRadixBinning32::apply(HornetClass& hornet,
-//                          const vid_t *      d_input,
-//                          int                num_vertices,
-//                          const Operator&    op) const noexcept {
-//     //static_assert(IsHornet<HornetClass>::value,
-//     //             "LogarthimRadixBinning32: paramenter is not an instance of Hornet Class");
-//     // prefixsum.resize(num_vertices + 1);
-//     // int ITEMS_PER_BLOCK = xlib::DeviceProperty
-//     //                       ::smem_per_block<vid_t>(BLOCK_SIZE);
-//     //assert(num_vertices < _work_size && "LogarthimRadixBinning32 (work queue) too small");
-
-//     cudaMemset(d_bins,0,33*sizeof(vid_t));
-
-//     if (d_input != nullptr) {
-//     kernel::computeWorkKernelLRB
-//         <<< xlib::ceil_div<BLOCK_SIZE>(num_vertices), BLOCK_SIZE >>>
-//         (hornet.device(), d_input, num_vertices, d_bins);
-//     } else {
-//     kernel::computeWorkKernelLRB
-//         <<< xlib::ceil_div<BLOCK_SIZE>(num_vertices), BLOCK_SIZE >>>
-//         (hornet.device(), num_vertices, d_bins);
-//     }
-
-//     kernel::binPrefixKernelLRB <<<1,32>>> (d_bins,d_binsPrefix);  
-//     uint32_t h_binsPrefix[33];
-//     cudaMemcpy(h_binsPrefix, d_binsPrefix,sizeof(uint32_t)*33, cudaMemcpyDeviceToHost);
-
-//     if (d_input != nullptr) {
-//         int rebinblocks = (num_vertices)/BLOCK_SIZE + (((num_vertices)%BLOCK_SIZE)?1:0);
-//         kernel::rebinKernelLRB<false><<<rebinblocks,BLOCK_SIZE>>>(hornet.device(),d_input,
-//             d_binsPrefix, d_lrbRelabled,num_vertices);
-
-//     } else {
-//         int rebinblocks = (num_vertices)/BLOCK_SIZE + (((num_vertices)%BLOCK_SIZE)?1:0);
-//         kernel::rebinKernelLRB<true><<<rebinblocks,BLOCK_SIZE>>>(hornet.device(),d_input,
-//             d_binsPrefix, d_lrbRelabled,num_vertices);
-//     }
-
-
-//     activeVertices = h_binsPrefix[20];
-//     if(activeVertices>0){
-//         kernel::fatWorkerLRB<<<activeVertices,blockSize,0,streams[1]>>>(hornet.device(), d_lrbRelabled, op, activeVertices,0);
-//     }
-
-//     const remainingFat=9;
-//     const int bi = 20+remainingFat-1;
-//     for(int i=1; i<remainingFat; i++){
-//         activeVertices = h_binsPrefix[20+i]-h_binsPrefix[19+i];
-//         if(activeVertices>0){
-//             kernel::fatWorkerLRB<<<activeVertices,blockSize,0,streams[i+1]>>>(hornet.device(), d_lrbRelabled, op, activeVertices,h_binsPrefix[19+i]);  
-//         }
-//         if(i==4)
-//             blockSize=128;
-//         if(i==6)
-//             blockSize=32;
-//     }
-
-//     const int smallBlockSize = 64;
-//     int smallVertices = num_vertices-h_binsPrefix[bi];
-//     int smallVerticesBlocks = (smallVertices)/smallBlockSize + ((smallVertices%smallBlockSize)?1:0);
-//     if(smallVerticesBlocks>0){                   
-//         kernel::skinnyWorkerLRB<<<smallVerticesBlocks,smallBlockSize,0,streams[STREAMS-1]>>>( hornet.device(), d_lrbRelabled, op, smallVertices,h_binsPrefix[bi]); 
-//     }
-//     cudaEventSynchronize(syncher);
-// }
